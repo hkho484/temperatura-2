fda64376774205c4f95cc64a0313cf39f5f186f2
hkho484 <133361308+hkho484@users.noreply.github.com>
2023-05-12 13:27:45 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    if (input.temperature() > 35) {
+        basic.showIcon(IconNames.Sad)
+    } else if (false) {
+    	
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
+})
